3bc91e64d103fd05830acafd2529f18a4212ca15
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-03-12 16:19:03 -0500

    Revert "disable adming temporarily"
    This reverts commit 9737e79ac6414d2c23e0281b7358b103277613ba.

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('app/', include('syfts.urls')),
     path('', include('landing_page.urls')),
